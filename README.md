a97c373322c1c8d19affa73050a3fe5d2824a36c
David Fekke <david@fekke.com>
2013-11-30 22:19:52 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# JaxPHP/JaxWEB Bootstrap sample
+
+This GitHub repo is for the examples used in the December 2013 JaxPHP on twitter bootstrap.
+
+Copyright 2013 David Fekke 
---
+++ footer.php
@@ -0,0 +1,15 @@
+    <div id="footer">
+        <div class="container">
+            <p class="text-muted credit">Example courtesy <a href="http://fekke.com">David Fekke</a>.</p>
+        </div>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+	<script>window.jQuery || document.write('<script src="/js/jquery-1.10.2.min.js"><\/script>')</script>
+	<script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
