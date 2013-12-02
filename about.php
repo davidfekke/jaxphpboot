6d9869f34fc04e7a022e0aaa70b92b3db589b53c
David Fekke <david@fekke.com>
2013-12-02 12:59:04 -0500

    Added some style changes to contact

---
+++ about.php
@@ -0,0 +1,23 @@
+
+
+
+<?php
+    $title = "About";
+    include 'header.php';
+
+?>
+
+    <div class="container">
+
+	<div class="page-header">
+	  <h1>About JaxPHP Bootstrap <small>Subtext for header</small></h1>
+	</div>
+      
+
+    </div> <!-- /container -->
+
+<?php
+
+    include 'footer.php';
+
+?>
--- contact.php
+++ contact.php
@@ -6,8 +6,17 @@
 
     <div class="container">
 
-      <h1>Contact Form</h1>
+		<div class="page-header">
+		  <h1>Contact Form <small>Subtext for contact</small></h1>
+		</div>
 
+		<div class="form-group">
+		
+		
+		
+		
+		</div>
+	
     </div> <!-- /container -->
 
 <?php
