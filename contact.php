e68d218dedc292e5bf58af7263e569da4553a775
davidfekke <david@fekke.com>
2013-12-02 21:11:53 -0500

    I added new groups to contact form

--- contact.php
+++ contact.php
@@ -9,8 +9,8 @@
 		  <h1>Contact Form <small>Subtext for contact</small></h1>
 		</div>
 
-		<div class="col-md-12">
-			<form role="form">
+		<div class="col-md-6">
+			<form role="form" class="form-horizontal">
 			  <div class="form-group">
 				<label for="exampleFirstName">First Name</label>
 				<input type="email" class="form-control" id="exampleFirstName" placeholder="First Name">
@@ -20,21 +20,23 @@
 				<input type="email" class="form-control" id="exampleLastName" placeholder="Last Name">
 			  </div>
 			  <div class="form-group">
-				<label for="exampleInputEmail1">Email address</label>
-				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+				<label for="email">Email</label>
+				<div class="input-group input-group-lg">
+				  <span class="input-group-addon">@</span>
+				  <input type="text" id="email" class="form-control" placeholder="email">
+				</div>
 			  </div>
 			  <div class="form-group">
 				<label for="exampleInputPassword1">Password</label>
 				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
 			  </div>
-			  
-			  <div class="checkbox">
-				<label>
-				  <input type="checkbox"> Check me out
-				</label>
+			  <div class="form-group">
+				<div class="btn-group">
+				  <button type="submit" class="btn btn-primary">Submit</button>
+				  <button class="btn btn-danger">Cancel</button>
+				</div>
 			  </div>
-			  <button type="submit" class="btn btn-primary">Submit</button>
-			  <button class="btn btn-danger">Cancel</button>
+			
 			</form>
 		</div>
 		
